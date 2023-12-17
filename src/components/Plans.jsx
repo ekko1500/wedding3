89d194778c52a6c84b15236df056907ae3d3971b
ekko1500 <ekko0499@gmail.com>
2023-12-17 14:32:35 +0630

    remove a div

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -7,15 +7,6 @@ function Plans() {
       className=" flex flex-col items-center text-white w-full gap-6"
       style={{ backgroundColor: "#292c2b" }}
     >
-      <img
-        width="45"
-        height="70"
-        src="https://lonehtoowedding.com/wp-content/uploads/2020/06/rose-icon-01.png"
-        alt=""
-        loading="lazy"
-      ></img>
-      <br />
-      <h2 className=" text-6xl font-bold">When & Where</h2>
       <br />
       <p className=" text-4xl">We love to see you soon</p>
       <br />
